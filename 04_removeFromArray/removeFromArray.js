f7d78530f90c9e47e919724569fa3e9c24f3cf43
nejjetc <nejjetc@gmail.com>
2022-02-18 23:35:00 +0700

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  let filteredArray = "";
 
+  filteredArray = array.filter(function (element) {
+    return args.indexOf(element) < 0;
+  });
+
+  return filteredArray;
 };
 
 // Do not edit below this line
