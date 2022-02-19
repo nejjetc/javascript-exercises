8406b5f0411d86e46426bb095fde030273f09b7d
nejjetc <nejjetc@gmail.com>
2022-02-19 16:10:31 +0700

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let total = "";
 
+  if (typeof a !== "number" || a < 0 || typeof b !== "number" || b < 0) {
+    total = "ERROR";
+  } else {
+    let smallNum = a < b ? a : b;
+    let largeNum = a > b ? a : b;
+
+    if (smallNum === largeNum) {
+      total = smallNum;
+    } else {
+      let subNum = smallNum - 1;
+      // n(n + 1)/2
+      total = (largeNum * (largeNum + 1)) / 2 - (subNum * (subNum + 1)) / 2;
+    }
+  }
+
+  return total;
 };
 
 // Do not edit below this line
