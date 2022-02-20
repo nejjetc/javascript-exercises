58465f0043e751ea1151b71a5726fb45dc5c28cf
nejjetc <nejjetc@gmail.com>
2022-02-20 15:52:45 +0700

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = 0;
+
+  if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+  }
+
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 0;
+  if (array.length > 0) {
+    total = 1;
+    for (let i = 0; i < array.length; i++) {
+      total *= array[i];
+    }
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
