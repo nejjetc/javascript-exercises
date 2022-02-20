ea53fa26f1a79bf7ad3eea3a4391e3aae4c1f16d
nejjetc <nejjetc@gmail.com>
2022-02-20 21:20:34 +0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  let number = parseInt(input);
 
+  if (Number.isNaN(number)) {
+    return;
+  }
+
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  let n1 = 0;
+  let n2 = 1;
+  let nextNumber = 0;
+
+  for (let i = 1; i <= number; i++) {
+    nextNumber = n1 + n2;
+    n1 = n2;
+    n2 = nextNumber;
+  }
+
+  return n1;
 };
 
 // Do not edit below this line
