9f87c8f5be51569f626ec0044f0c2d65d1581581
nejjetc <nejjetc@gmail.com>
2022-02-20 18:16:34 +0700

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regx = /[\W_]/g;
+  let newStr = str.toLowerCase().replace(regx, "");
+  return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
