06571539ed1440b51ce86e0f2925a1e60e127879
nejjetc <nejjetc@gmail.com>
2022-02-20 13:14:34 +0700

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  let temp = ((num - 32) * 5) / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  let temp = (num * 9) / 5 + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
