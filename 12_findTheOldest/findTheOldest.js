acc5f4e0b1777774d22ff47ea3bb2c2c1829aace
nejjetc <nejjetc@gmail.com>
2022-02-20 23:30:46 +0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let theOdestPerson = people.reduce(function (previousPerson, currentPerson) {
+    const currentYear = new Date().getFullYear();
 
+    let previousPersonAge =
+      (previousPerson.yearOfDeath ?? currentYear) - previousPerson.yearOfBirth;
+    let currentPersonAge =
+      (currentPerson.yearOfDeath ?? currentYear) - currentPerson.yearOfBirth;
+
+    return previousPersonAge > currentPersonAge
+      ? previousPerson
+      : currentPerson;
+  });
+
+  return theOdestPerson;
 };
 
 // Do not edit below this line
